ef0afc1426f70049e1ccce9c25df74d9a3868633
Haegnim <84562770+Haegnim@users.noreply.github.com>
2021-08-30 18:26:21 +0900

    0830 메인페이지 슬라이드 기능 구현, 햄버거 클릭 구현

---
+++ js/main.js
@@ -0,0 +1,100 @@
+function mainconClick() {
+  const todaycard = document.querySelectorAll(".card");
+  const pre = document.querySelector(".pre");
+  const nex = document.querySelector(".nex");
+  // const cardX = card[0].offsetWidth;
+  let offset = 0;
+  pre.addEventListener("click", function () {
+    offset -= 1;
+    mainconSlide(offset);
+  });
+  nex.addEventListener("click", function () {
+    offset -= 1;
+    mainconSlide(offset);
+  });
+}
+mainconClick();
+
+function mainconSlide(offset) {
+  const todaycard = document.querySelectorAll(".card");
+  const cardX = todaycard[0].offsetHeight;
+  const todayList = document.querySelector(".today-list");
+  const movepoint = offset % todaycard.length;
+
+  todayList.style.transform = `translateY(${cardX * movepoint}px)`;
+  navActive(movepoint);
+}
+
+function navActive(num) {
+  const navbtn = document.querySelectorAll(".nav-button li a");
+  const navbtnArr = Array.from(navbtn);
+
+  navbtnArr.forEach((a) => {
+    a.classList.remove("active");
+  });
+  // console.log(num);
+  navbtn[Math.abs(num)].classList.add("active");
+}
+
+function consBoxX() {
+  const conTwoBox = document.querySelector(".con-twobox");
+  const consBox = document.querySelectorAll(".cons-box");
+  const conTwoBoxX = conTwoBox.offsetWidth;
+  // console.log(conTwoBoxX / 2);
+  const consBoxArr = Array.from(consBox);
+  consBoxArr.forEach((div) => {
+    div.style.width = `${conTwoBoxX / 2}px`;
+  });
+}
+consBoxX();
+function resize() {
+  window.addEventListener("resize", function () {
+    consBoxX();
+  });
+}
+resize();
+
+function consBoxClick() {
+  let offset = 0;
+  const morePre = document.querySelector(".more-pre");
+  const moreNex = document.querySelector(".more-nex");
+  morePre.addEventListener("click", function () {
+    offset += 1;
+    consBoxSlide(offset);
+  });
+  moreNex.addEventListener("click", function () {
+    offset -= 1;
+    consBoxSlide(offset);
+  });
+}
+consBoxClick();
+function consBoxSlide(offset) {
+  const conTwoBox = document.querySelector(".con-twobox");
+  const consBox = document.querySelectorAll(".cons-box");
+  const conList = document.querySelector(".con-list");
+  const conTwoBoxX = conTwoBox.offsetWidth;
+  const movepoint = offset % (consBox.length / 2);
+
+  conList.style.transform = `translateX(${conTwoBoxX * movepoint}px)`;
+}
+// function navClick() {
+//   const navbtn = document.querySelectorAll(".nav-button li a");
+//   for (let i = 0; i <= navbtn.length; i++) {
+//     navbtn[i].classList.remove("active");
+//     navbtn[i].addEventListener("click", function () {
+//       navbtn[i].classList.add("active");
+//     });
+//   }
+// }
+// navClick();
+
+function hambergerClick() {
+  const hamberger = document.querySelector(".th-line");
+  // const tabnav = document.querySelector(".hide-nav");
+
+  hamberger.addEventListener("click", function () {
+    // tabnav.classList.toggle("on");
+    hamberger.classList.toggle("on");
+  });
+}
+hambergerClick();
